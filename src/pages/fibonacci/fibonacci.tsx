b4c37a107445f2ec0c794890836262baa88bfefe
Zhenia <zhenia1985@gmail.com>
2022-10-20 16:44:32 +0700

    fix: repair fibo

--- src/pages/fibonacci/fibonacci.tsx
+++ src/pages/fibonacci/fibonacci.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useEffect, useState } from "react"
+import { FC, useEffect, useState } from "react"
 import { SolutionLayout } from "components/ui/solution-layout/solution-layout"
 import { Button } from "components/ui/button/button"
 import { Circle } from "components/ui/circle/circle"
@@ -17,28 +17,29 @@ const waitSleep = (ms: number) => {
 
 export const FibonacciPage: FC = () => {
 
-   //получаемый импут
-   const [input, setInput] = useState<string>()
+   const [input, setInput] = useState<string>("")
    //обрабатываемый массив
    const [arr, setArr] = useState<Array<number>>([])
    // флаг загрузки
    const [loading, setLoading] = useState<boolean>(false)
 
-   const [test, setTest] = useState<any>([])
+   const [test, setTest] = useState<Array<number>>([])
 
    useEffect(() => {
-      if (test.length !== arr.length)
-         setTimeout(() => {
+      if (test.length !== arr.length) {
+         // setTimeout(() => {
+         //   setTest([...test, arr[test.length]])
+         // },  SHORT_DELAY_IN_MS)
+         waitSleep(SHORT_DELAY_IN_MS).then(() => {
             setTest([...test, arr[test.length]])
-         }, 300)
-      else {
+         })
+      } else {
+         setLoading(false)
          setInput("")
       }
-
    }, [test, arr])
 
    const solution = (n: string) => {
-
       const number = parseInt(n)
       if (number === 1) {
          setArr([0])
@@ -47,13 +48,13 @@ export const FibonacciPage: FC = () => {
          setArr([0, 1])
       }
 
-      let arr = [0, 1]
+      const arr = [0, 1]
       let i = 2
       while (i < number) {
-         //  await waitSleep(SHORT_DELAY_IN_MS)
+         // await waitSleep(SHORT_DELAY_IN_MS)
          let element = arr[i - 2] + arr[i - 1]
          arr.push(element)
-         i++;
+         i++
          setArr([...arr])
       }
    }
@@ -62,10 +63,6 @@ export const FibonacciPage: FC = () => {
       e.preventDefault()
       setLoading(true)
       solution(input ?? "можно было через if, но я не хочу, ts не понимает что здесь всегда что то есть")
-      console.log("тута")
-      setInput("")
-      //    ff()
-      setLoading(false)
       setTest([])
    }
 
@@ -73,13 +70,13 @@ export const FibonacciPage: FC = () => {
       <SolutionLayout title="Последовательность Фибоначчи">
          <form className={styles.form} onSubmit={clickHandler}>
             <div className={styles.input}>
-               <Input placeholder="Введите число" type="number" isLimitText={true} max={19} onChange={e => setInput(e.currentTarget.value)} />
-               <Button type="submit" text="Рассчитать" disabled={!input || parseInt(input) > 19 || parseInt(input) < 1} isLoader={loading} />
+               <Input placeholder="Введите число" type="number" isLimitText={true} max={19} value={input} onChange={(e) => setInput(e.currentTarget.value)} />
+               <Button type="submit" text="Рассчитать" disabled={input === "" || parseInt(input) > 19 || parseInt(input) < 1} isLoader={loading} />
             </div>
          </form>
          <ul className={styles.ul}>
             {test &&
-               test.map((e: any, i: any) =>
+               test.map((e, i) =>
                   <li className={styles.li} key={i} ><Circle letter={e} index={i + 1} /></li>
                )}
          </ul>
