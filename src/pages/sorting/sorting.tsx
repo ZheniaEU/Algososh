d1b617fcc9bfc18671e6a75bec521fac45aa55bd
Zhenia <zhenia1985@gmail.com>
2022-10-21 19:55:53 +0700

    feat: sort before coloring

--- src/pages/sorting/sorting.tsx
+++ src/pages/sorting/sorting.tsx
@@ -10,13 +10,17 @@ import { ElementStates } from "types/element-states"
 
 import styles from "./sorting.module.css"
 
+type Tuple = [number, string]
+
+type Render = (arr: Array<Tuple>) => void
+
 export const SortingPage: FC = () => {
 
-   const [arr, setArr] = useState<Array<[number, string]>>([])
+   const [arr, setArr] = useState<Array<Tuple>>([])
    const [loading, setLoading] = useState<boolean>(false)
    const [sort, setSort] = useState<"selection" | "bubble">("selection")
 
-   const getArray = (min: number = 0, max: number = 100, length: number, i: number = 0, arr: Array<[number, string]> = []) => {
+   const getArray = (min: number = 0, max: number = 100, length: number, i: number = 0, arr: Array<Tuple> = []) => {
 
       while (i < length) {
          arr.push([randomInteger(min, max), ElementStates.Default])
@@ -48,43 +52,54 @@ export const SortingPage: FC = () => {
       })
    }
 
-   const swap = async (arr: Array<[number, string]>, i: number, z: number) => {
-
+   const swap = async (arr: Array<Tuple>, i: number, z: number) => {
+      if (arr[i][0] === arr[z][0])
+         return
       arr[i][0] ^= arr[z][0]
       arr[z][0] ^= arr[i][0]
       arr[i][0] ^= arr[z][0]
-      await waitSleep(300)
+      await waitSleep(100)
+      console.log(arr)
    }
 
-   const selectedSort = async (arr: Array<[number, string]>, rule: string, min?: number) => {
+   const selectedSort = async (arr: Array<Tuple>, rule: string, render: Render) => {
 
-      for (let i = 0; i <= arr.length - 1; i++) {
-         min = i
+      const isAsc = rule === "ascending"
 
-         for (let j = i; j <= arr.length - 1; j++) {
-            rule === "ascending" ? arr[j][0] < arr[min][0] ? min = j : min = min : arr[j][0] > arr[min][0] ? min = j : min = min
-         }
+      for (let i = 0; i < arr.length; i++) {
+         let min = i
 
-         rule === "ascending" ? arr[i][0] > arr[min][0] ? await swap(arr, i, min) : null : arr[i][0] < arr[min][0] ? await swap(arr, i, min) : null
-         setArr([...arr])
+         for (let j = i; j < arr.length; j++)
+            if (isAsc && arr[j][0] < arr[min][0] || !isAsc && arr[j][0] > arr[min][0])
+               min = j
+
+         if (isAsc && arr[i][0] > arr[min][0] || !isAsc && arr[i][0] < arr[min][0])
+            await swap(arr, i, min)
+
+         render([...arr])
       }
       return arr
    }
 
-   const bubbleSort = (rule: string) => {
-      const array = arr
-      return console.log(array, rule)
+   const bubbleSort = async (arr: Array<Tuple>, rule: string, render: Render) => {
 
+      const isAsc = rule === "ascending"
+
+      for (let i = 0; i < arr.length; i++) {
+         for (let j = 0; j < arr.length - i - 1; j++) {
+            if (isAsc && arr[j][0] > arr[j + 1][0] || !isAsc && arr[j][0] < arr[j + 1][0])
+               await swap(arr, j, j + 1)
+
+            render([...arr])
+         }
+      }
+      return arr
    }
 
-   const sortingArray = async (sort: string, rule: string) => {
-
-      const array = arr
-      const selected = selectedSort(array, rule)
-      const bubble = bubbleSort(rule)
+   const sortingArray = async (sort: "selection" | "bubble", rule: string) => {
 
       setLoading(true)
-      sort === "selection" ? setArr(await selected) : bubble
+      sort === "selection" ? setArr(await selectedSort(arr, rule, setArr)) : setArr(await bubbleSort(arr, rule, setArr))
       setLoading(false)
    }
 
@@ -93,7 +108,7 @@ export const SortingPage: FC = () => {
          <div className={styles.container} >
             <div className={styles.radio}>
                <RadioInput label="Выбор" name="sort" disabled={loading} onChange={() => setSort("selection")} checked={sort === "selection"} />
-               <RadioInput label="Пузырёк" name="sort" disabled={loading} onChange={() => setSort("bubble")} />
+               <RadioInput label="Пузырёк" name="sort" disabled={loading} onChange={() => setSort("bubble")} checked={sort === "bubble"} />
             </div>
             <Button text="По возрастанию" sorting={Direction.Ascending} extraClass={styles.button_sort} disabled={loading} onClick={() => sortingArray(sort, "ascending")} />
             <Button text="По убыванию" sorting={Direction.Descending} extraClass={styles.button_sort} disabled={loading} onClick={() => sortingArray(sort, "descending")} />
@@ -140,6 +155,22 @@ export const SortingPage: FC = () => {
 //          setArr([...arr])
 //          arr = [...arr]
 //       }
+//    }
+//    return arr
+// }
+
+
+// const selectedSort = async (arr: TArray, rule: string, render: (arr: TArray) => void, min?: number) => {
+
+//    for (let i = 0; i <= arr.length - 1; i++) {
+//       min = i
+
+//       for (let j = i; j <= arr.length - 1; j++) {
+//          rule === "ascending" ? arr[j][0] < arr[min][0] ? min = j : "" : arr[j][0] > arr[min][0] ? min = j : -Infinity
+//       }
+
+//       rule === "ascending" ? arr[i][0] > arr[min][0] ? await swap(arr, i, min) : null : arr[i][0] < arr[min][0] ? await swap(arr, i, min) : NaN
+//       render([...arr])
 //    }
 //    return arr
 // }
