f029736ee836de8f3ca3fe70b9af9548c766e016
Zhenia <zhenia1985@gmail.com>
2022-11-18 05:51:31 +0700

    feat: reverse string fix

--- cypress/e2e/string.spec.cy.js
+++ cypress/e2e/string.spec.cy.js
@@ -3,15 +3,14 @@ describe("reverse string", () => {
    it("empty input value and disable button", () => {
       cy.visit("/reverse")
       cy.get("input").should("have.value", "")
-      cy.get("button").should("be.disabled").as("test")
+      cy.get("button").should("be.disabled")
    })
 
-   it("reverse string", async () => {
+   it("reverse string", () => {
       cy.get("input").type("Aprel")
       cy.get("input").should("have.value", "Aprel")
       cy.get("button").should("be.enabled").contains("Развернуть").click()
       cy.get("[data-testid=\"circle\"]").should("have.length", 5).as("circle")
-      cy.wait(500)
       cy.get("@circle").should(($e) => {
          expect($e.eq(0)).to.have.text("l").and.css("border-color", "rgb(210, 82, 225)")
          expect($e.eq(1)).to.have.text("p").and.css("border-color", "rgb(0, 50, 255)")
@@ -19,7 +18,6 @@ describe("reverse string", () => {
          expect($e.eq(3)).to.have.text("e").and.css("border-color", "rgb(0, 50, 255)")
          expect($e.eq(4)).to.have.text("A").and.css("border-color", "rgb(210, 82, 225)")
       })
-      cy.wait(500)
       cy.get("@circle").should(($e) => {
          expect($e.eq(0)).to.have.text("l").and.css("border-color", "rgb(127, 224, 81)")
          expect($e.eq(1)).to.have.text("e").and.css("border-color", "rgb(210, 82, 225)")
@@ -27,7 +25,6 @@ describe("reverse string", () => {
          expect($e.eq(3)).to.have.text("p").and.css("border-color", "rgb(210, 82, 225)")
          expect($e.eq(4)).to.have.text("A").and.css("border-color", "rgb(127, 224, 81)")
       })
-      cy.wait(500)
       cy.get("@circle").should(($e) => {
          expect($e.eq(0)).to.have.text("l").and.css("border-color", "rgb(127, 224, 81)")
          expect($e.eq(1)).to.have.text("e").and.css("border-color", "rgb(127, 224, 81)")
@@ -35,7 +32,6 @@ describe("reverse string", () => {
          expect($e.eq(3)).to.have.text("p").and.css("border-color", "rgb(127, 224, 81)")
          expect($e.eq(4)).to.have.text("A").and.css("border-color", "rgb(127, 224, 81)")
       })
-      cy.wait(500)
       cy.get("@circle").should(($e) => {
          expect($e.eq(0)).to.have.text("l").and.css("border-color", "rgb(127, 224, 81)")
          expect($e.eq(1)).to.have.text("e").and.css("border-color", "rgb(127, 224, 81)")
@@ -44,7 +40,7 @@ describe("reverse string", () => {
          expect($e.eq(4)).to.have.text("A").and.css("border-color", "rgb(127, 224, 81)")
       })
       cy.get("input").should("have.value", "")
-      cy.get("@test")
+      cy.get("button").should("be.disabled")
    })
 })
 // "rgb(0, 50, 255)" // синий дефолт
